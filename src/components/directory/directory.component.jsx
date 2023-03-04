6e3081f74f282be5e33eb78e46e65b9443422445
SuyashPatil-29 <112413583+SuyashPatil-29@users.noreply.github.com>
2023-03-04 19:18:52 +0530

    Started the initial ui of the first page

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import categories from "../category-item/categories"
+import CategoryItem from "../category-item/CategoryItem.component"
+import "./Directory.styles.scss"
+
+const Directory = ()=>{
+    return(<div className='directory-container'>
+    {categories.map((category)=>{
+      return(<CategoryItem key = {category.id} category={category}/>)
+    })}
+    </div>
+    )
+}
+
+export default Directory

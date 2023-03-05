0c6806bc14a35f21985c10e835b4dd6dd4d51884
SuyashPatil-29 <112413583+SuyashPatil-29@users.noreply.github.com>
2023-03-05 12:07:29 +0530

    Added routes and started the nav bar component.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import Directory from "./components/directory/directory.component";
+import Home from "./routes/home/home.component";
+import { Routes, Route , Outlet} from "react-router-dom";
 
+const Navigation = ()=>{
+  return( <div>
+    <div>
+    <h1>I am the navigation bar</h1>
+    </div>
+    <Outlet />
+  </div>)
+}
+
+const Shop = ()=>{
+  return(
+    <h1>I am the Shop Section</h1>
+)
+}
 
 
 
 const App= ()=> {
   return (
-    <Directory />
+   <Routes>
+   <Route path="/" element={<Navigation />}>
+    <Route index element={<Home />} />
+    <Route path="shop" element={<Shop />} />
+   </Route>
+   </Routes>
   );
 }
 
